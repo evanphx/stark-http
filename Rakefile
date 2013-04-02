c4c7172eab1cee49ea515c98ab592973c0988112
Evan Phoenix <evan@phx.io>
2013-04-01 17:47:55 -0700

    And then there was stark-http

---
+++ Rakefile
@@ -0,0 +1,11 @@
+# -*- ruby -*-
+
+require 'rubygems'
+require 'hoe'
+
+Hoe.spec 'stark-http' do
+  developer('Evan Phoenix', 'evan@phx.io')
+  dependency 'stark', '< 2.0.0'
+end
+
+# vim: syntax=ruby
---
+++ lib/stark/http.rb
@@ -0,0 +1,57 @@
+require 'stark'
+
+class Stark::HTTP
+  VERSION = '1.0.0'
+
+  class ClientTransport < Thrift::BaseTransport
+    def initialize(url)
+      @url = URI url
+      @headers = {'Content-Type' => 'application/x-thrift'}
+      @outbuf = ""
+
+      @http = Net::HTTP.new @url.host, @url.port
+      @http.use_ssl = @url.scheme == "https"
+      @http.set_debug_output $stderr
+    end
+
+    def open?; true end
+    def read(sz); @inbuf.read sz end
+    def write(buf); @outbuf << buf end
+
+    def add_headers(headers)
+      @headers = @headers.merge(headers)
+    end
+
+    RETRY_ATTEMPTS = 10
+
+    def flush
+      attempts = 0
+
+      @http.start unless @http.started?
+
+      begin
+        resp = @http.post(@url.request_uri, @outbuf, @headers)
+        attempts = 0
+      rescue StandardError => e
+        @http.finish
+        
+        raise e if attempts > RETRY_ATTEMPTS
+
+        attempts += 1
+
+        @http.start
+
+        retry
+      end
+
+      @inbuf = StringIO.new resp.body
+      @outbuf = ""
+    end
+  end
+
+  def self.open(url, interface)
+    trans = Thrift::HTTP::ClientTransport.new(url)
+    proto = Thrift::BinaryProtocolFactory.new.get_protocol trans
+    interface.new proto
+  end
+end
